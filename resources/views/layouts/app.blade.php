f70b4794f71e9bc3ab0c537118ef717436b9de24
rubo-taype <rtaype@pronatel.gob.pe>
2026-07-10 13:07:24 -0500

    Modo presentación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,16 @@
 
     <title>PETD - Plataforma de Evaluación para la Transformación Digital</title>
 
-    @vite(['resources/css/app.css','resources/js/app.js'])
+ 
+
+
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
+            <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
 
